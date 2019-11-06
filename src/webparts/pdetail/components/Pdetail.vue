752e952e977a4c0ec40b08c85a4f5510586d4e48
duanvuminh <duanvuminh@gmail.com>
2019-11-06 14:56:36 +0900

    fixed layout too length at page load

--- src/webparts/pdetail/components/Pdetail.vue
+++ src/webparts/pdetail/components/Pdetail.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app :id="$style.keep">
     <v-content app>
-      <v-container>
+      <div class="cs_container">
         <v-layout align-center justify-center>
           <router-view />
         </v-layout>
-      </v-container>
+      </div>
     </v-content>
   </v-app>
 </template>
@@ -24,6 +24,43 @@
   }
   html {
     overflow: auto !important;
+  }
+  .cs_container {
+      margin: 0 auto;
+    }
+  /* Extra small devices (phones, 600px and down) */
+  @media only screen and (max-width: 600px) {
+    .cs_container {
+      max-width: 95%;
+    }
+  }
+
+  /* Small devices (portrait tablets and large phones, 600px and up) */
+  @media only screen and (min-width: 600px) {
+    .cs_container {
+      max-width: 90%;
+    }
+  }
+
+  /* Medium devices (landscape tablets, 768px and up) */
+  @media only screen and (min-width: 768px) {
+    .cs_container {
+      max-width: 80%;
+    }
+  }
+
+  /* Large devices (laptops/desktops, 992px and up) */
+  @media only screen and (min-width: 992px) {
+    .cs_container {
+      max-width: 70%;
+    }
+  }
+
+  /* Extra large devices (large laptops and desktops, 1200px and up) */
+  @media only screen and (min-width: 1200px) {
+    .cs_container {
+      max-width: 60%;
+    }
   }
 }
 #keep {
